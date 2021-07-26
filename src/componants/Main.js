e4bc7243bce452b233cf3e3e2b313a9cc46af647
khair eldeen zakaria shkokany <shkokany98@gmail.com>
2021-07-26 17:09:41 +0300

    solution demo 2

--- src/componants/Main.js
+++ src/componants/Main.js
@@ -1,28 +1,37 @@
 
 import React from 'react';
 import Hornedbeast from './Hornedbeast';
-import Hornedbeast1 from './Hornedbeast1';
-import Hornedbeast2 from './Hornedbeast2';
+import data from './assests/data.json'
+
+// import Hornedbeast1 from './Hornedbeast1';
+// import Hornedbeast2 from './Hornedbeast2';
 
 
 
 class Main extends React.Component {
 
+ 
+    
+
     render() {
 
+
+     
         return (
 
-            <>
+             data.map((element) => {
 
-                <Hornedbeast />
-                <Hornedbeast1 />
-                <Hornedbeast2 />
+                return (
+    
+                    <Hornedbeast image_url={element.image_url} title={element.title} description = {element.description}/>
+                    
 
-
-            </>
-
+                                       );
+            })
+        
+     
         )
+
     }
 }
-
 export default Main;
