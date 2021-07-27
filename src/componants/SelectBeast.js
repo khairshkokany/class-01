e7ffe0ad17b6897395e760aea0c757a79f79a998
khair eldeen zakaria shkokany <shkokany98@gmail.com>
2021-07-27 23:30:24 +0300

    70% solution

---
+++ src/componants/SelectBeast.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
+
+
+
+
+
+class SelectBeast extends React.Component {
+
+    exitModal = () =>{
+        this.props.hidden(false)
+    }
+    render() {
+
+        return (
+
+            <>
+
+
+                <Modal show={this.props.showModal} onHide={this.exitModal}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>{this.props.title}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <img
+                            src={this.props.image_url}
+                            alt={this.props.title}
+                        />
+                        <p>{this.props.description}
+                        </p>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.exitModal}>
+                            Close
+                        </Button>
+                        
+                    </Modal.Footer>
+                </Modal>
+            </>
+        );
+    }
+}
+
+export default SelectBeast;
+
